7cdc2711d39305749a37b1aeb6bdb0cea9b7bb3d
Kishore-1022 <kishoresmart229@gmail.com>
2023-10-25 23:59:42 +0530

    enabeling delete function in cart

--- web/src/components/Cart/Cart.js
+++ web/src/components/Cart/Cart.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
+
 
 const Cart = (props) => {
-    const cartItem= <ul className={classes['cart-items']}>{[{id:'c1',name:'Sushi', amount:2,price:12.99}].map(i=><li>{i.name}</li>)}</ul>
+  const cartCtx=useContext(CartContext);
+  const totalAmount=cartCtx.totalAmount.toFixed(2);
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemsRemoveHandler=id=>{
+    cartCtx.removeItem(id)
+       
+  };
+  const cartItemHandler = item=>{
+    console.log(item)
+    cartCtx.addItem({...item,amount:1})
+  };
+    const cartItem= <ul className={classes['cart-items']}>
+      {cartCtx.items.map(i=><CartItem 
+      key={i.id} 
+      i={i} 
+      onRemove={cartItemsRemoveHandler.bind(null,i.id)} 
+      onAdd={cartItemHandler.bind(null,i)}
+      />)}
+      </ul>
   return (
 
     <Modal onClose={props.onClose}>
         {cartItem}
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>${totalAmount}</span>
         </div>
         <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
         </div> 
     </Modal>
   )
--- web/src/components/Meals/MealItem/MealItemForm.js
+++ web/src/components/Meals/MealItem/MealItemForm.js
@@ -6,9 +6,9 @@ import { useRef, useState } from 'react'
 const MealItemForm = (props) => {
   const [amountIsValid,setAmountIsValid]= useState(true);
   const amountInputRef=useRef();
+  
   const submitHandler=event=>{
     event.preventDefault();
-    
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
     if(enteredAmount.trim().length===0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
